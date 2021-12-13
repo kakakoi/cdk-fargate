ceb8eeda8b96a64d099c7165ddf0c90011f9117e
kakakoi <56661650+kakakoi@users.noreply.github.com>
2021-12-13 14:04:56 +0900

    [add] alb stack

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { CdkStack } from '../lib/cdk-stack';
+import { AlbStack } from '../lib/alb-stack';
 
 const app = new cdk.App();
 new CdkStack(app, 'CdkStack');
+new AlbStack(app, 'AlbStack');
---
+++ cdk/lib/alb-stack.ts
@@ -0,0 +1,32 @@
+import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
+import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns'
+import * as ecs from 'aws-cdk-lib/aws-ecs'
+import * as ecr from 'aws-cdk-lib/aws-ecr'
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import { Construct } from 'constructs';
+
+export class AlbStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, 'Vpc', {})
+
+    const cluster = new ecs.Cluster(this, 'Cluster', {
+      vpc,
+    })
+
+    const repository = ecr.Repository.fromRepositoryName(this, 'repo', 'my-repo-name')
+    const loadBalancedFargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'Service', {
+      cluster,
+      memoryLimitMiB: 1024,
+      cpu: 512,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromEcrRepository(repository)
+      },
+    });
+
+    loadBalancedFargateService.targetGroup.configureHealthCheck({
+      path: "/custom-health-path",
+    });
+  }
+}
