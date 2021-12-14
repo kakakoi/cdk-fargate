c23342123ec9f9b7607284c6e85c88642021a3ba
kakakoi <56661650+kakakoi@users.noreply.github.com>
2021-12-14 20:05:45 +0900

    1

--- cdk/test/cdk.test.ts
+++ cdk/test/cdk.test.ts
@@ -5,13 +5,12 @@ import * as Cdk from '../lib/ecr-stack';
 test('SQS Queue and SNS Topic Created', () => {
   const app = new cdk.App();
   // WHEN
-  const stack = new Cdk.CdkStack(app, 'MyTestStack');
+  const stack = new Cdk.EcrStack(app, 'MyTestStack');
   // THEN
 
   const template = Template.fromStack(stack);
 
-  template.hasResourceProperties('AWS::SQS::Queue', {
+  template.hasResourceProperties('AWS::ECR::Repository', {
     VisibilityTimeout: 300
   });
-  template.resourceCountIs('AWS::SNS::Topic', 1);
 });
